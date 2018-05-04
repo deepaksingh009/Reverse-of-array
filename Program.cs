56fd9089b60b4fa466b128c8e3e00a43edc09e9f
Deepak Singh <deepaks@newsprintgroup.com>
2018-05-04 16:35:53 +0530

    Design Pattern

--- Program.cs
+++ Program.cs
@@ -29,27 +29,27 @@ namespace ConsoleApp2
             //nums.Insert(4);
             //nums.Insert(5);
 
-            //BinaryTree b = new BinaryTree();
-            //b.insert(1);
-            //b.insert(2);
-            //b.insert(3);
-            //b.insert(4);
-            //b.insert(5);
-            //b.insert(6);
-            //b.insert(7);
-            //b.insert(8);
-            //b.insert(9);
-            //b.insert(10);
-            //b.insert(11);
-            //Console.WriteLine("The Inorder Traversal:\n");
-            //b.inOrderTraversal();
-            //Console.WriteLine("\n\nThe Preorder Traversal:\n");
-            //b.preOrderTraversal();
-            //Console.WriteLine("\n\nThe Postorder Traversal:\n");
-            //b.postOrderTraversal();
-            //Console.WriteLine();
-            //Console.WriteLine("Deepak singh");
-            //Console.ReadKey();
+            BinaryTree b = new BinaryTree();
+            b.insert(1);
+            b.insert(2);
+            b.insert(3);
+            b.insert(4);
+            b.insert(5);
+            b.insert(6);
+            b.insert(7);
+            b.insert(8);
+            b.insert(9);
+            b.insert(10);
+            b.insert(11);
+            Console.WriteLine("The Inorder Traversal:\n");
+            b.inOrderTraversal();
+            Console.WriteLine("\n\nThe Preorder Traversal:\n");
+            b.preOrderTraversal();
+            Console.WriteLine("\n\nThe Postorder Traversal:\n");
+            b.postOrderTraversal();
+            Console.WriteLine();
+            Console.WriteLine("Deepak singh");
+            Console.ReadKey();
 
 
             FactoryMethod factoryMethod = new FactoryMethod();
