821963c9abcd389333a602d5c359241d71ca44a8
Deepak Singh <dpksngh480@gmail.com>
2018-05-09 13:40:30 +0530

    Design Patterns

---
+++ DesignPattern/Creational Pattern/Factory Pattern/Interface/Interface1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.DesignPattern.Interface
+{
+    public interface Itestinginterface
+    {
+        int add();
+
+        int subtract();
+
+		int multiply();
+
+    }
+}
