a7d467c99f89061d6e19f08ccc7637038adf72db
Deepak Singh <dpksngh480@gmail.com>
2018-05-11 17:15:07 +0530

    binary tree

--- Program.cs
+++ Program.cs
@@ -72,14 +72,22 @@ namespace ConsoleApp2
 
 
 
-            BinaryTrees tree = new BinaryTrees();
+            BinaryTree tree = new BinaryTree();
+            tree.root = new tNode(1);
+            tree.root.left = new tNode(2);
+            tree.root.right = new tNode(3);
+            tree.root.left.left = new tNode(4);
+        
+            tree.root.left.right = new tNode(5);
+        
+            tree.root.right.left = new tNode(6);
+          
+            tree.root.right.right = new tNode(7);
+         
 
-            tree.root = new Nodes(1);
-            tree.root.left = new Nodes(2);
-            tree.root.right = new Nodes(3);
-            tree.root.left.left = new Nodes(4);
-            tree.root.left.right = new Nodes(5);
-            Console.WriteLine(tree.maxDepth(tree.root));
+
+
+            tree.MorrisTraversal(tree.root);
 
             Console.ReadKey();
 
@@ -89,50 +97,112 @@ namespace ConsoleApp2
 
 
     }
-    // Java program to find height of tree
 
-    // A binary tree node
-    public class Nodes
+    public class tNode
     {
         public int data;
-        public Nodes left, right;
+        public tNode left, right;
 
-        public Nodes(int item)
+        public tNode(int item)
         {
             data = item;
             left = right = null;
         }
     }
 
-    public class BinaryTrees
+    public class BinaryTree
     {
-        public Nodes root;
+        public tNode root;
 
-        /* Compute the "maxDepth" of a tree -- the number of 
-           nodes along the longest path from the root node 
-           down to the farthest leaf node.*/
-        public int maxDepth(Nodes node)
+        /* Function to traverse binary tree without recursion and 
+           without stack */
+        public void MorrisTraversal(tNode root)
         {
-            if (node == null)
-                return 0;
-            else
+            tNode current, pre;
+
+            if (root == null)
+                return;
+
+            current = root;
+            while (current != null)
             {
-                /* compute the depth of each subtree */
-                int lDepth = maxDepth(node.left);
-                int rDepth = maxDepth(node.right);
-
-                /* use the larger one */
-                if (lDepth > rDepth)
-                    return (lDepth + 1);
+                if (current.left == null)
+                {
+                    Console.WriteLine(current.data + " ");
+                    current = current.right;
+                }
                 else
-                    return (rDepth + 1);
-            }
+                {
+                    /* Find the inorder predecessor of current */
+                    pre = current.left;
+                    while (pre.right != null && pre.right != current)
+                        pre = pre.right;
+
+                    /* Make current as right child of its inorder predecessor */
+                    if (pre.right == null)
+                    {
+                        pre.right = current;
+                        current = current.left;
+                    }
+
+                    /* Revert the changes made in if part to restore the 
+                       original tree i.e.,fix the right child of predecssor*/
+                    else
+                    {
+                        pre.right = null;
+                        Console.WriteLine(current.data + " ");
+                        current = current.right;
+                    }   /* End of if condition pre->right == NULL */
+
+                } /* End of if condition current->left == NULL*/
+
+            } /* End of while */
+
         }
+        // Java program to find height of tree
+
+        // A binary tree node
+        //public class Nodes
+        //{
+        //    public int data;
+        //    public Nodes left, right;
+
+        //    public Nodes(int item)
+        //    {
+        //        data = item;
+        //        left = right = null;
+        //    }
+        //}
+
+        //public class BinaryTrees
+        //{
+        //    public Nodes root;
+
+        //    /* Compute the "maxDepth" of a tree -- the number of 
+        //       nodes along the longest path from the root node 
+        //       down to the farthest leaf node.*/
+        //    public int maxDepth(Nodes node)
+        //    {
+        //        if (node == null)
+        //            return 0;
+        //        else
+        //        {
+        //            /* compute the depth of each subtree */
+        //            int lDepth = maxDepth(node.left);
+        //            int rDepth = maxDepth(node.right);
+
+        //            /* use the larger one */
+        //            if (lDepth > rDepth)
+        //                return (lDepth + 1);
+        //            else
+        //                return (rDepth + 1);
+        //        }
+        //    }
+        //}
     }
+
+
 }
-
-
-
 
 
 
