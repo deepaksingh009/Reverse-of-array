6443b6c27aa577fb0c6fb6fd91e7449449d6a552
Deepak Singh <dpksngh480@gmail.com>
2018-05-16 17:11:01 +0530

    binary tree

--- Program.cs
+++ Program.cs
@@ -7,56 +7,10 @@ namespace ConsoleApp2
     {
         static void Main()
         {
-            MorriseInordertreetraversal tree = new MorriseInordertreetraversal();
-            tree.root = new tNode(1);
-            tree.root.left = new tNode(2);
-            tree.root.right = new tNode(3);
-            tree.root.left.left = new tNode(4);
-
-            tree.root.left.right = new tNode(5);
-
-            tree.root.right.left = new tNode(6);
-
-            tree.root.right.right = new tNode(7);
-
-
-            //tree.root.left.left.left = new tNode(8);
-            //tree.root.left.left.right = new tNode(9);
-
-            //tree.root.left.right.left = new tNode(10);
-            //tree.root.left.right.right = new tNode(11);
-
-            //tree.root.right.left.left = new tNode(12);
-            //tree.root.right.left.right = new tNode(13);
-
-            //tree.root.right.right.left = new tNode(14);
-            //tree.root.right.right.right = new tNode(15);
-
-            BinarySearchTree consoleApp2 = new BinarySearchTree();
-            consoleApp2.inOrder(tree.root);
-
-            Console.WriteLine();
             Console.ReadKey();
-
-
-
-           
-
-
         }
 
-        private void inOrder(tNode node)
-        {
-            if (node == null)
-            {
-                return;
-            }
-
-            
-            inOrder(node.left);
-            Console.WriteLine(node.data);
-            inOrder(node.right);
-        }
+        
     }
 
    
