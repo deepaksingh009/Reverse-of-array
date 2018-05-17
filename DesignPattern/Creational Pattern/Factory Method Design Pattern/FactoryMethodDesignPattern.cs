84dfd0e33065a6901ae243a4b361e138465c42ba
deepak singh <dpksngh480@gmail.com>
2018-05-17 22:48:53 +0530

    new binary tree class

---
+++ DesignPattern/Creational Pattern/Factory Method Design Pattern/FactoryMethodDesignPattern.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.DesignPattern.Creational_Pattern.Factory_Method_Design_Pattern
+{
+	public class FactoryMethodDesignPattern
+	{
+	}
+}
---
+++ DesignPattern/Creational Pattern/Factory Pattern/twonosum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp2.DesignPattern.Interface;
+
+namespace ConsoleApp2.DesignPattern.Creational_Pattern.Factory_Pattern
+{
+  public  class twonosum: Itestinginterface
+
+	{
+		public int add()
+		{
+			return 1 + 2;
+		}
+    }
+	public class Threenosum : Itestinginterface
+	{
+		public int add()
+		{
+			return 1 + 2 + 3;
+		}
+	}
+}
