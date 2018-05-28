a75966fc3a8c7b39da6058759b1368cd83bed8cc
Deepak Singh <dpksngh480@gmail.com>
2018-05-28 15:51:22 +0530

    Pattern

--- Patterns/Patterns.cs
+++ Patterns/Patterns.cs
@@ -56,26 +56,71 @@ namespace ConsoleApp2.Patterns
         public void LReverse()
         {
             int counter = 1;
-            for (int i = 10; i > 0; i--)    
+            int no = 3;
+            for (int i = no; i > 0; i--)    
             {
                 for (int j = 0; j <= i; j++)
                 {
-                    if (j != i)
+                    if (j != i-1)
                     {
                         Console.Write(" ");
                     }
                     else
                     {
-                        for (int k = 0; k < counter; k++)
+                        for (int k = 1; k <= counter; k++)
                         {
-                            Console.Write("*");
-                            
-                            
+                            Console.Write(k);
+                        }
+                        for (int f = 1; f < counter; f++)
+                        {
+                            Console.Write(f);
                         }
                         counter++;
                     }
-                   
                 }
+                Console.WriteLine();
+
+            }
+          counter = 1;
+            for (int i = 3; i > 0; i--)
+            {
+                for (int j = 0; j < counter; j++)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int k = 1; k <= i-1; k++)
+                {
+                    Console.Write(k);
+                }
+
+                for (int k = counter; k > 0; k--)
+                {
+                    Console.Write(k);
+                }
+                counter++;
+
+                Console.WriteLine();
+
+            }
+        }
+
+        public void ReversePyramid()
+        {
+            int counter = 1;
+            for (int i = 10; i > 0; i--)
+            {
+                for (int j = 0; j < counter; j++)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int k = 0; k < i-1; k++)
+                {
+                    Console.Write("*");
+                }
+                counter++;
+
                 Console.WriteLine();
 
             }
