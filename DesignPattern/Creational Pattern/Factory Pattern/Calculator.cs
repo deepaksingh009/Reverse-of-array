33c42b4e0df3ce21bd5d4d5fe5cbf85f969999e1
deepaksingh009 <dpksngh480@gmail.com>
2018-05-29 21:29:11 +0530

    Calculatorclass

---
+++ DesignPattern/Creational Pattern/Factory Pattern/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp2.DesignPattern.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.DesignPattern
+{
+    class Calculator : Itestinginterface
+    {
+
+        public int add()
+        {
+            return 1 + 1;
+        }
+
+        public int subtract()
+        {
+            return 1 - 1;
+        }
+
+		public int multiply()
+		{
+			return 1 - 1;
+		}
+	}
+}
