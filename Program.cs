ea41946c735453dc625482d61ee0380fc6782b3c
deepak singh <dpksngh480@gmail.com>
2018-07-02 21:19:50 +0530

    Add File to angular repo

--- Program.cs
+++ Program.cs
@@ -1,27 +1,29 @@
 ﻿using ConsoleApp2.DataStructure.BinaryTree;
 using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp2
 {
     public class BinarySearchTree
     {
-		void start()
-		{
-			long[] a1 = { 3, 4, 5 };
-			long[] a2 = fix(a1);
-			Console.WriteLine(a1[0] + a1[1] + a1[2] + " ");
-			Console.WriteLine(a2[0] + a2[1] + a2[2]);
-		}
+		private LinkedList<int> adjLists;
+		//void start()
+		//{
+		//	long[] a1 = { 3, 4, 5 };
+		//	long[] a2 = fix(a1);
+		//	Console.WriteLine(a1[0] + a1[1] + a1[2] + " ");
+		//	Console.WriteLine(a2[0] + a2[1] + a2[2]);
+		//}
 
-		long[] fix(long[] a3)
-		{
-			a3[1] = 7;
-			return a3;
-		}
+		//long[] fix(long[] a3)
+		//{
+		//	a3[1] = 7;
+		//	return a3;
+		//}
 		static void Main()
         {
 			BinarySearchTree p = new BinarySearchTree();
-			p.start();	
+			//p.start();	
 
 			//Node root = new Node(1);
 			//root.Left = new Node(2);
