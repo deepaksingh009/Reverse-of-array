26754471ba6e1a91c6593fd45c66d56d5d323b2d
deepak singh <dpksngh480@gmail.com>
2018-07-02 22:40:40 +0530

    Add File to angular repo

--- Program.cs
+++ Program.cs
@@ -22,9 +22,19 @@ namespace ConsoleApp2
 		//}
 		static void Main()
         {
-			BinarySearchTree p = new BinarySearchTree();
-			//p.start();	
-
+			var tune = new LinkedList<string>();
+			tune.AddFirst("do"); // do
+			tune.AddLast("so"); // do - so
+			tune.AddAfter(tune.First, "re"); // do - re- so
+			tune.AddAfter(tune.First.Next, "mi"); // do - re - mi- so
+			tune.AddBefore(tune.Last, "fa"); // do - re - mi - fa- so
+			tune.RemoveFirst(); // re - mi - fa - so
+			tune.RemoveLast(); // re - mi - fa
+			LinkedListNode<string> miNode = tune.Find("mi");
+			tune.Remove(miNode); // re - fa
+			tune.AddFirst(miNode); // mi- re - fa
+			foreach (string s in tune) Console.WriteLine(s);
+			
 			//Node root = new Node(1);
 			//root.Left = new Node(2);
 			//root.Right = new Node(3);
