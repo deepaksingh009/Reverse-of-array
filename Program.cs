ed59b01d37a6b75809304c0de3e46835d0efd5f1
deepak singh <dpksngh480@gmail.com>
2018-07-04 23:25:23 +0530

    Graph

--- Program.cs
+++ Program.cs
@@ -7,19 +7,7 @@ namespace ConsoleApp2
     public class BinarySearchTree
     {
 		private LinkedList<int> adjLists;
-		//void start()
-		//{
-		//	long[] a1 = { 3, 4, 5 };
-		//	long[] a2 = fix(a1);
-		//	Console.WriteLine(a1[0] + a1[1] + a1[2] + " ");
-		//	Console.WriteLine(a2[0] + a2[1] + a2[2]);
-		//}
-
-		//long[] fix(long[] a3)
-		//{
-		//	a3[1] = 7;
-		//	return a3;
-		//}
+		
 		static void Main()
         {
 			var tune = new LinkedList<string>();
@@ -149,7 +137,21 @@ namespace ConsoleApp2
 
 }
 
+#region array middle value
+//void start()
+//{
+//	long[] a1 = { 3, 4, 5 };
+//	long[] a2 = fix(a1);
+//	Console.WriteLine(a1[0] + a1[1] + a1[2] + " ");
+//	Console.WriteLine(a2[0] + a2[1] + a2[2]);
+//}
 
+//long[] fix(long[] a3)
+//{
+//	a3[1] = 7;
+//	return a3;
+//}
+#endregion
 
 
 
